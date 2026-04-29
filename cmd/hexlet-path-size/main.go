e104382a4469296879b05f5893dd7c55c73599d0
admiral <admiralgamesstudio@gmail.com>
2026-04-29 11:15:58 +0300

    next step

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -6,12 +6,41 @@ import (
 	"os"
     "context"
 	"github.com/bobkoffandrei/go-project-242/pathsize"
+	"fmt"
+	"log"
 )
 
-func main(){
-(&cli.Command{}).Run(context.Background(), os.Args)
 
 
-GetPathSize("~/go-project-242", false, false, false)
+func main() {
 
+    cmd := &cli.Command{
+
+        Name:  "ADS",
+
+        Usage: "Analyze Disk Size",
+
+        Action: func(ctx context.Context, c *cli.Command) error {
+		result, err := pathsize.GetPathSize(c.Args().Get(0), false, false, false)
+        	if err != nil {
+		//fmt.Println("Error:", err)
+		return err
+	}
+        fmt.Printf("%sB\t%s\n", result, c.Args().Get(0))
+            return nil
+        },
+        // Флаги, которые можно передать программе
+        Flags: []cli.Flag{
+            &cli.StringFlag{
+                
+            },
+        },
+    }
+
+    // Запускаем приложение, передаём аргументы командной строки
+    if err := cmd.Run(context.Background(), os.Args); err != nil {
+        log.Fatal(err)
+    }
+
+	
 }
--- pathsize/pathsize.go
+++ pathsize/pathsize.go
@@ -3,19 +3,112 @@ package pathsize
 import (
 	"os"
 	"fmt"
-//	"strconv"
+//"strconv"
 )
 
 
 func GetPathSize(path string, recursive, human, all bool) (string, error) {
-	info, err := os.Lstat(path)
+	var result string
+	var err error
+
+switch isDirCheck(path){
+	case false:
+	result, err = getFileSize(path)
+	case true:
+	result, err = getDirSize(path)	
+}
+	return result, err
+}
+
+func isDirCheck(dir string) bool {
+	fileInfo, err := os.Stat(dir)
 	if err != nil {
 		fmt.Println("Error:", err)
-		
-		return "", err
-
+		return false
 	}
-	fmt.Println("Размер:", info.Size())
-	//result := strconv.FormatInt(info.Size, 10) 
-	return "", nil
+	return fileInfo.IsDir()
+}
+
+func getFileSize(dir string) (string, error){
+	info, err := os.Lstat(dir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", err
+	}
+	r := fmt.Sprintf("%d", info.Size())
+	return r, nil
+}
+
+func getDirSize(dir string) (string, error){
+var sumSize int64 = 0
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", err
+	}
+	for _, file := range files {
+
+		tempDir := dir + "/" + file.Name()
+
+
+	fileInfo2, err := os.Stat(tempDir)
+
+    if (!fileInfo2.IsDir()) {
+			info, err := os.Lstat(tempDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", err
+	}
+	sumSize += info.Size()
+	}
+	_, err = os.Lstat(tempDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", err
+	}
+	}
+	//fmt.Printf("%dB\t%s\n", sumSize, dir)
+	r := fmt.Sprintf("%d", sumSize)
+
+	return r, nil
+}
+
+func printFileSize(dir string) {
+	info, err := os.Lstat(dir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Printf("%dB\t%s\n", info.Size(), dir)
+}
+
+func printDirSize(dir string) {
+var sumSize int64 = 0
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return 
+	}
+	for _, file := range files {
+
+		tempDir := dir + "/" + file.Name()
+
+
+	fileInfo2, err := os.Stat(tempDir)
+
+    if (!fileInfo2.IsDir()) {
+			info, err := os.Lstat(tempDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return 
+	}
+	sumSize += info.Size()
+	}
+	_, err = os.Lstat(tempDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	}
+	fmt.Printf("%dB\t%s\n", sumSize, dir)
 }
